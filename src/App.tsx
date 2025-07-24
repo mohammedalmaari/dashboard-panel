ddb336415b0e8c8fe23d1305beac54eb4cdfd8f5
mohammedalmaari <145218003+mohammedalmaari@users.noreply.github.com>
2025-07-24 11:28:31 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import '../src/styles/App.css'
+import "../src/styles/index.css";
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-500 p-3 border border-black'>mohamed saeed almaari</h1>
+    </>
+  );
+}
+
+export default App
